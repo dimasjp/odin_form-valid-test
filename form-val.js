bbd8750be3444240f37d73507b8ebb31d4fee902
diims.el <diims.el@gmail.com>
2023-02-21 11:02:03 +0700

    Initial commits

---
+++ form-val.js
@@ -0,0 +1,6 @@
+const form = document.getElementById('#form');
+const emailInput = document.getElementById('#email');
+const regionInput = document.getElementById('#region');
+const zipInput = document.getElementById('#zip-code');
+const passInput = document.getElementById('#pass');
+const confirmPassInput = document.getElementById('#pass-confirm');
